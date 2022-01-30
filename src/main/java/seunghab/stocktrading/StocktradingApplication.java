4af09cbca9d104c9a104efca759efb68f44a62e7
seunghabaik <slug0326@naver.com>
2022-01-30 18:20:04 +0900

    Initial commit

---
+++ src/main/java/seunghab/stocktrading/StocktradingApplication.java
@@ -0,0 +1,13 @@
+package seunghab.stocktrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seunghab/stocktrading/StocktradingApplicationTests.java
@@ -0,0 +1,13 @@
+package seunghab.stocktrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
